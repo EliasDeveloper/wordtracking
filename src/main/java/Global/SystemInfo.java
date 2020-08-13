ee4c3e45a333270bd4ccc8741c668c934f9e307a
eliasaprende <contact@fast.com>
2020-08-13 18:09:57 -0400

    the version of the program has changed

--- src/main/java/Global/SystemInfo.java
+++ src/main/java/Global/SystemInfo.java
@@ -3,7 +3,7 @@ package Global;
 public class SystemInfo {
 
 
-    public static String SystemVersion = " 1.0 beta";
+    public static String SystemVersion = " 1.1 released";
 
     public static String DefaultNamePrinting = "WordFromDB.txt";
 
