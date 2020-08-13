aad4427d2e87c8b3a006245936d5d3310b1c3ed5
eliasaprende <contact@fast.com>
2020-08-13 18:08:56 -0400

    the validation of the location has been added

--- src/main/java/model/Print/WordFilePrinter.java
+++ src/main/java/model/Print/WordFilePrinter.java
@@ -1,5 +1,6 @@
 package model.Print;
 
+import Global.SystemInfo;
 import model.dto.Word;
 
 import java.io.*;
@@ -10,7 +11,6 @@ import java.util.Observable;
 public class WordFilePrinter extends Observable {
 	 private List<Word> wordlist = null;
 	 private String location;
-
 
 	public WordFilePrinter(List<Word> wordlist, String location){
 		 if(Objects.isNull(wordlist)) throw new NullPointerException("The word list cannot be null. ");
@@ -24,8 +24,22 @@ public class WordFilePrinter extends Observable {
 
 	public void startPrinting() throws IOException {
 		 Writer FileWriter = null;
-		 FileWriter = new BufferedWriter(new FileWriter(new File(this.location)));
+		 FileWriter = new BufferedWriter(new FileWriter(new File(getLocation(location))));
 		 printingfileloop(FileWriter);
+	}
+
+	private String getLocation(String command){
+		 switch (command){
+			  case "desktop":
+			  	 return getPropertywithloc() + "Desktop" + File.pathSeparator + SystemInfo.DefaultNamePrinting;
+			  case "document":
+			  	 return  getPropertywithloc() + "Documents" + File.pathSeparator + SystemInfo.DefaultNamePrinting;
+			  	 default:
+			  	 	 return command;
+		 }
+	}
+	private String getPropertywithloc(){
+		 return System.getProperty("user.home") + File.pathSeparator;
 	}
 
 
@@ -38,8 +52,6 @@ public class WordFilePrinter extends Observable {
 		 setChanged();
 		 notifyObservers(wordlist.size());
 	}
-
-
 	public void setWordlist(List<Word> wordlist){
 		 if(Objects.isNull(wordlist)) throw  new NullPointerException("the word list cannot be null");
 		 this.wordlist = wordlist;
