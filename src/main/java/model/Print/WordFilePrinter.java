d6b942e7381947581f562567b7d2e01d8cddc4d6
eliasaprende <contact@fast.com>
2020-08-15 18:14:28 -0400

    corrections of the printing method

--- src/main/java/model/Print/WordFilePrinter.java
+++ src/main/java/model/Print/WordFilePrinter.java
@@ -1,6 +1,6 @@
 package model.Print;
 
-import Global.SystemInfo;
+import model.dto.PrintObject;
 import model.dto.Word;
 
 import java.io.*;
@@ -10,47 +10,49 @@ import java.util.Observable;
 
 public class WordFilePrinter extends Observable {
 	 private List<Word> wordlist = null;
-	 private String location;
+	 private PrintObject pobj;
 
-	public WordFilePrinter(List<Word> wordlist, String location){
+	public WordFilePrinter(List<Word> wordlist, PrintObject location){
 		 if(Objects.isNull(wordlist)) throw new NullPointerException("The word list cannot be null. ");
 		 this.wordlist = wordlist;
-		 this.location = location;
+		 this.pobj = location;
 	}
 
-	public WordFilePrinter(String location){
-		 this.location = location;
+	public WordFilePrinter(PrintObject location){
+		 this.pobj = location;
 	}
 
-	public void startPrinting() throws IOException {
-		 Writer FileWriter = null;
-		 FileWriter = new BufferedWriter(new FileWriter(new File(getLocation(location))));
-		 printingfileloop(FileWriter);
+	public void startPrinting()  {
+		 try {
+			  Writer writer;
+			  writer = new BufferedWriter(new FileWriter(new File(getLocation(pobj.getLocation()))));
+			  writer.write("#startid at "+ pobj.getIdInicial() + "\n#endid at "+ pobj.getIdFinal() + "\n");
+			  writer.write("\t\tEnglish to spanish vocabulary  \n");
+			  int  counter = 1;
+			  for (Word word : wordlist) {
+				   writer.write("("+ counter + ")=" + word.getEnglishWord() + "=" + word.getSpanishWord() + "\n");
+				   counter = counter + 1;
+			  }
+			  writer.close();
+			  setChanged();
+			  notifyObservers(wordlist.size());
+		 }catch (IOException error){
+			  System.out.println(error.getMessage());
+		 }
 	}
 
 	private String getLocation(String command){
 		 switch (command){
 			  case "desktop":
-			  	 return getPropertywithloc() + "Desktop" + File.pathSeparator + SystemInfo.DefaultNamePrinting;
+			  	 return getPropertywithloc() + "Desktop" + "\\" +pobj.getFilename() ;
 			  case "document":
-			  	 return  getPropertywithloc() + "Documents" + File.pathSeparator + SystemInfo.DefaultNamePrinting;
+			  	 return  getPropertywithloc() + "Documents" + "\\"+ pobj.getFilename();
 			  	 default:
 			  	 	 return command;
 		 }
 	}
 	private String getPropertywithloc(){
-		 return System.getProperty("user.home") + File.pathSeparator;
-	}
-
-
-	private void printingfileloop(Writer writer) throws IOException{
-		 writer.write("English to spanish dictionary \n");
-		 for(Word word: wordlist){
-		 	 writer.write(word.getEnglishWord() + "=" + word.getSpanishWord() + "\n");
-		 }
-		 writer.close();
-		 setChanged();
-		 notifyObservers(wordlist.size());
+		 return System.getProperty("user.home") + "\\";
 	}
 	public void setWordlist(List<Word> wordlist){
 		 if(Objects.isNull(wordlist)) throw  new NullPointerException("the word list cannot be null");
